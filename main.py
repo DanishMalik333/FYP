dbc85d660912da4c209f3c135dd2161eb991a69a
Danish <danish.117@outlook.com>
2020-11-12 14:27:03 +0500

    Image crop to 180*280px

--- main.py
+++ main.py
@@ -10,10 +10,25 @@ from hardware import ser_com
 #img = cv2.imread('image.jpg')
 #cv2.imshow('image.jpg', img)
 #key = cv2.waitKey(1000)
-#print (img.shape)
+#print (img.shape)import cv2
+import os
+import time
+os.system('cls')
+from image_read import take_img
+#from hardware import ser_com
+
+#object_type = int(input('Enter the type of object: '))
+take_img()
+img = cv2.imread('image.jpg')
+img = img[145:315, 415:685]
+#img = cv2.rectangle(img, (160,140), (280,180), 255, 2)
+cv2.imshow('img', img)
+key = cv2.waitKey(10000)
+print(img.shape)
 
 
-time.sleep(0.5)
-ser_com('90+90+90+0')
-time.sleep(0.5)
-ser_com('180+90+90+0')
+#time.sleep(0.5)
+#ser_com('90+90+90+0')
+#time.sleep(0.5)
+#ser_com('180+90+90+0')
+
