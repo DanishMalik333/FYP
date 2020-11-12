f365d0867879f1db0da9049de1bc102d026e8e4e
Danish <danish.117@outlook.com>
2020-11-12 12:51:51 +0500

    main file for multiple angles serial communication

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import cv2
 import os
+import time
 os.system('cls')
-from image_read import take_img
+#from image_read import take_img
+from hardware import ser_com
+
+#object_type = int(input('Enter the type of object: '))
+#take_img()
+#img = cv2.imread('image.jpg')
+#cv2.imshow('image.jpg', img)
+#key = cv2.waitKey(1000)
+#print (img.shape)
 
 
-object_type = int(input('Enter the type of object: '))
-take_img()
-img = cv2.imread('image.jpg')
-cv2.imshow('image.jpg', img)
-key = cv2.waitKey(1000)
-
+time.sleep(0.5)
+ser_com('90+90+90+0')
+time.sleep(0.5)
+ser_com('180+90+90+0')
